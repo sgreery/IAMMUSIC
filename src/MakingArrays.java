90b19b32936ed007d4c10a0723319a0d5b613353
sgreery <samuel_greer27@milton.edu>
2025-02-20 18:19:28 -0500

    made more arrays

--- src/MakingArrays.java
+++ src/MakingArrays.java
@@ -1,5 +1,5 @@
 public class MakingArrays {
-    int [] Carti = new int [3];
+    int [] Carti = new int [10];
     public static void main(String[] args) {
         System.out.println("Hello world!");
         MakingArrays make = new MakingArrays();
@@ -15,11 +15,31 @@ public class MakingArrays {
         Carti[0] = 5;
         Carti[1] = 10;
         Carti[2] = Carti[1]*3;
+        Carti[3] = 17;
+        Carti[4] = 24;
+        Carti[5] = 3;
+        Carti[6] = 4;
+        Carti[7] = 14;
+        Carti[8] = 26;
+        Carti[9] = 16;
+
+
+
+
+
+
         printArray();
     }
     public void printArray(){
         System.out.println(Carti[0]);
         System.out.println(Carti[1]);
         System.out.println(Carti[2]);
+        System.out.println(Carti[3]);
+        System.out.println(Carti[4]);
+        System.out.println(Carti[5]);
+        System.out.println(Carti[6]);
+        System.out.println(Carti[7]);
+        System.out.println(Carti[8]);
+        System.out.println(Carti[9]);
     }
 }
